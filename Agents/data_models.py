8f6b11ea123d09810fb12385543497383f11a67a
JaszczurGra <julian.mikolajczak@student.put.poznan.pl>
2025-03-22 17:39:08 +0100

    Model logic change

--- Agents/data_models.py
+++ Agents/data_models.py
@@ -2,12 +2,16 @@
 from uagents import Model
 
 
+
+#Agents decisions 
 class ResponseAgent(Model):
+    frame = 0
     sell = 0
     store = 0
     buy = 0
 
 
+#Data from simulation
 class QueryEnv(Model):
     bought: int
     sold: int
--- Agents/model.py
+++ Agents/model.py
@@ -9,16 +9,16 @@ class ModelAgent():
         agent = Agent(seed="khavaioghgjabougrvbosubvisgvgjfkf", **agent_kwargs)
         agent.storage.set("frame",0)
 
-        def logic(input: QueryEnv) -> ResponseAgent:
-            return ResponseAgent(sell=0, store=0, buy=0)
 
-        agent.logic = logic
 
         @agent.on_message(QueryEnv, replies=ResponseAgent)
         async def recieve_enviroment(ctx: Context, _sender, message: QueryEnv):
-            response = await agent.logic(message)
+            response = self.logic(message)
             response.frame = agent.storage.get("frame")
             agent.storage.set("frame",response.frame+1)
             await ctx.send(_sender, response)
 
         self.agent = agent
+    
+    def logic(self,input: QueryEnv) -> ResponseAgent:
+        return ResponseAgent(sell=0, store=0, buy=0)
