0d181b0102886a4791568317959b4504a740d921
Dawid64 <dawid.siera@gmail.com>
2025-03-22 17:01:49 +0100

    changed agent

--- Agents/model.py
+++ Agents/model.py
@@ -1,24 +1,24 @@
 # Imports
-from uagents import Agent, Context,Model
+from uagents import Agent, Context, Model
 
 from simulation_compat_layer import QueryEnv
 
+
 class ResponseAgent(Model):
     sell = 0
-    store  =0 
+    store = 0
     buy = 0
 
+
 class ModelAgent():
-    agent = Agent(seed="khavaioghgjabougrvbosubvisgvgjfkf")
+    def __init__(self) -> None:
+        agent = Agent(seed="khavaioghgjabougrvbosubvisgvgjfkf")
 
+        @agent.on_message(QueryEnv, replies=ResponseAgent)
+        async def recieve_enviroment(ctx: Context, _sender, message: QueryEnv):
+            await ctx.send(_sender, await agent.logic(message))
 
+        async def logic(input: QueryEnv) -> ResponseAgent:
+            await ResponseAgent(sell=0, store=0, buy=0)
 
-    @agent.on_message(QueryEnv,replies=ResponseAgent)
-    async def recieve_enviroment(ctx: Context, _sender, message: QueryEnv):
-        await ctx.send(_sender,await ModelAgent.logic(message))
-
-
-    async def logic(input: QueryEnv) -> ResponseAgent:
-        await ResponseAgent(sell=0,store=0,buy=0)
-
-
+        self.agent = Agent
