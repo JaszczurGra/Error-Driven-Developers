5a896c0594ec25e6e6e29e9a000a7c9fe71ebf97
JaszczurGra <julian.mikolajczak@student.put.poznan.pl>
2025-03-22 17:33:38 +0100

    Asynchroous models updating environment

--- Agents/enviroment.py
+++ Agents/enviroment.py
@@ -14,6 +14,8 @@ class Environment():
     def __init__(self) -> None:
         self.environment = Agent(name='Environment', seed="khavaioghgjabougrvbosubvisgvgjfkf",
                                  endpoint="https://localhost:4443")
+        self.environment.storage.set("frame_counter", [0])
+        self.environment.storage.set("current_frame", 0)
         self.agents: list[Agent] = [ModelAgent(agent_kwargs={'name': f'Agent_{i}'}).agent for i in range(3)]
         self.simulation = EnvAgent().agent
         self.simulation.endpoint = self.environment.address
@@ -31,8 +33,22 @@ class Environment():
 
         @self.environment.on_message(ResponseAgent)
         async def receive_agent(ctx: Context, _sender, message):
-
             print(message)
+            received_frame = message.frame
+            counter = self.environment.storage.get("frame_counter")
+            while len(counter) <= received_frame:
+                counter.append(0)
+            counter[received_frame] += 1    
+
+            current_frame = self.environment.storage.get("current_frame")   
+            if counter[current_frame] == len(self.agents):
+                print('Updatuj stan ', current_frame)
+                self.environment.storage.set("current_frame", current_frame+1)
+
+
+
+            self.environment.storage.set("frame_counter", counter)
+
             return
 
     def run(self) -> None:
--- Agents/model.py
+++ Agents/model.py
@@ -5,6 +5,7 @@ from simulation_compat_layer import QueryEnv
 
 
 class ResponseAgent(Model):
+    frame = 0 
     sell = 0
     store = 0
     buy = 0
@@ -13,6 +14,7 @@ class ResponseAgent(Model):
 class ModelAgent():
     def __init__(self, agent_kwargs: dict) -> None:
         agent = Agent(seed="khavaioghgjabougrvbosubvisgvgjfkf", **agent_kwargs)
+        agent.storage.set("frame",0)
 
         async def logic(input: QueryEnv) -> ResponseAgent:
             return ResponseAgent(sell=0, store=0, buy=0)
@@ -21,6 +23,9 @@ class ModelAgent():
 
         @agent.on_message(QueryEnv, replies=ResponseAgent)
         async def recieve_enviroment(ctx: Context, _sender, message: QueryEnv):
-            await ctx.send(_sender, await agent.logic(message))
+            response = await agent.logic(message)
+            response.frame = agent.storage.get("frame")
+            agent.storage.set("frame",response.frame+1)
+            await ctx.send(_sender, response)
 
         self.agent = agent
